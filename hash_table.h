fcb4c34ddc8fe1d5bc2e6711e2b2253d7109a7bc
Beren Millidge <bmillidgework@gmail.com>
2018-03-22 17:09:14 +0000

    Add files via upload

---
+++ hash_table.h
@@ -0,0 +1,5 @@
+// this is the api that the hash table tries to implement
+
+void ht_insert(ht_hash_table* ht, const char* key, const char* value);
+char* ht_search(ht_hash_table* ht, const char* key);
+void ht_delete(ht_hash_table* ht, const char* key);
