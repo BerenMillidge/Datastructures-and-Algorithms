0409deac1b4e6e277cee9b8a32e016c241abf205
Beren Millidge <bmillidgework@gmail.com>
2018-03-30 20:50:59 +0100

    Add files via upload

--- bloom_filter.py
+++ bloom_filter.py
@@ -35,10 +35,59 @@ class BloomFilter(set):
 	def __contains__(self, item):
 		out = True
 		for ii in range(self.hash_count):
-			index = mm3.hash(item,ii) %self.size)
+			index = mm3.hash(item,ii) %self.size
 			if self.bit_array[index]==0:
 				out=False
 		return out
 
 		# means of expansion/improvement- counting bloom filters,
 		# and scalable bloom filters!
+
+
+#my own simple implementatio nof this
+class CountingBloomFilter(BloomFilter):
+
+	def __init__(self, size, hash_count):
+		super(CountingBloomFilter, self).__init__()
+		self.bit_array = bitarray(size)
+		self.bit_array.setall(0)
+		self.size=size
+		self.hash_count= hash_count
+
+
+		#adding is simple - just incremenet the counts each time
+	def add(self, item):
+		for ii in range(self.hash_count):
+			index = mm3.hash(item,ii) % self.size
+			self.bit_array[index] +=1
+		return self
+
+	def __contains__(self,item):
+		out = True
+		for ii in range(self.hash_count):
+			index = mm3.hash(item,ii) %self.size
+			if self.bit_array[index]==0:
+				out=False
+				#dont' need to do the whole loop if false
+				return out
+		return out
+
+	def remove(self,item):
+		#this is the tricky part, remove only if it is in there, which means that all
+		# the items are there, and this will result ni some false negatives
+		# and decrement there
+		#you can't get the item back, just a true/false
+		indices = []
+		for ii in range(self.hash_count):
+			index = mm3.hash(item,ii) %self.size
+			if self.bit_array[index] ==0:
+				# no point removing, so retrun
+				return False
+			#greater than 1 so can be removed
+			indices.append(index)
+
+		#if succeeds now decrement
+		for index in indices:
+			self.bit_array[index] -=1
+		return True
+
